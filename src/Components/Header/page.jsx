3b270504808d9791fafdbc2754cffe0c477d711f
Mahliyo <gasandigital13@gmail.com>
2026-07-21 17:21:37 +0500

    header

--- src/Components/Header/page.jsx
+++ src/Components/Header/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 export default function Header() {
   return (
-    <div>
+    <div className='flex items-center justify-between'>
         <div className="container">
             <a href="#">Logo</a>
             <ul>
