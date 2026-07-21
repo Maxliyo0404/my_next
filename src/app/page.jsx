ff7d329efcf90ff323b59cb5ab9612a40b127097
Mahliyo <gasandigital13@gmail.com>
2026-07-21 19:36:23 +0500

    header about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Hero from '@/Components/Hero/page'
 export default function Home() {
   return (
     <div>
-      
+      <main>
+        <Hero/>
+      </main>
+     
     </div>
   )
 }
