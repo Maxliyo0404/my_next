84244e084b5aa5fbe54768aee8671d447fdb0871
Mahliyo <gasandigital13@gmail.com>
2026-07-21 17:09:32 +0500

    my_next header star

--- src/Components/Header/page.jsx
+++ src/Components/Header/page.jsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
-
 export default function Header() {
   return (
     <div>
         <div className="container">
-            
+            <a href="#">Logo</a>
+            <ul>
+                <li><Link href="/about">About</Link></li>
+            </ul>
         </div>
     </div>
   )
