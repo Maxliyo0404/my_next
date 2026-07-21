b65888bf1b08ee127f800d04532c549a4e2f3f24
Mahliyo <gasandigital13@gmail.com>
2026-07-21 17:24:58 +0500

    header

--- src/Components/Header/page.jsx
+++ src/Components/Header/page.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 import React from 'react'
 export default function Header() {
   return (
-    <div className='flex items-center justify-between'>
-        <div className="container">
+    <div className=''>
+        <div className="container  flex items-center justify-between">
             <a href="#">Logo</a>
-            <ul>
+            <ul className='flex items-center gap-4'>
                 <li><Link href="/about">About</Link></li>
                 <li><Link href="/contact">Contact</Link></li>
                 <li><Link href="/services">Services</Link></li>
