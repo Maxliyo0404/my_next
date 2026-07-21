12a7d7a7238831bea2cb1c8cefa69042f67ccc70
Mahliyo <gasandigital13@gmail.com>
2026-07-21 19:18:32 +0500

    header

--- src/Components/Header/page.jsx
+++ src/Components/Header/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='w-full  bg-blue-950'>
-        <div className="container  flex items-center justify-between px-5 py-4">
+        <div className="container mx-auto  flex items-center justify-between px-5 py-4">
             <a href="#" className='text-white text-3xl'>Logo</a>
             <ul className='flex items-center gap-4 text-2xl text-white'>
                 <li><Link href="/about">About</Link></li>
--- src/app/contactPage/page.jsx
+++ src/app/contactPage/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ContactPage() {
   return (
-    <div>
+    <div >
         <h1>Men contactman</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
 
