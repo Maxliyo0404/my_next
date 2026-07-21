d1ca5dc76f19ca9f42df4cbb88b50a7fc55b961d
Mahliyo <gasandigital13@gmail.com>
2026-07-21 18:51:54 +0500

    header

--- src/Components/Header/page.jsx
+++ src/Components/Header/page.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import React from 'react'
 export default function Header() {
   return (
-    <header>
-        <div className="container  flex items-center justify-between bg-blue-950 px-5 py-4">
+    <header className='w-full  bg-blue-950'>
+        <div className="container  flex items-center justify-between px-5 py-4">
             <a href="#" className='text-white text-3xl'>Logo</a>
             <ul className='flex items-center gap-4 text-2xl text-white'>
                 <li><Link href="/about">About</Link></li>
