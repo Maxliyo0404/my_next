f0acb05f583fff2f5176f9b152d1adf230112b9c
Mahliyo <gasandigital13@gmail.com>
2026-07-21 19:32:16 +0500

    header about

--- src/Components/Hero/page.jsx
+++ src/Components/Hero/page.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>page</div>
+    <div>
+        <h1>hero start</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
+        <button>bossish</button>
+    </div>
   )
 }
