93aefc127ff15d8423e28e1856e7b92e1e79cd50
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:27:04 +0500

    hero star

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ServicesPage() {
   return (
     <div>
-       <div className="container w-full bg-emerald-800 mb-10 text-2xl text-white">
+       <div className="container w-full bg-emerald-800 mb-10 text-2xl">
         <h1 className='text-4xl text-white' >Men servicesman</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
     </div>
