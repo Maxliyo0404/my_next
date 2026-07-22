504bbfb01552ca6c01653bcded8586be44e8f36f
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:34:20 +0500

    hero star

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,7 +5,7 @@ export default function ContactPage() {
     <div >
       <div className="container h-auto w-full bg-amber-800 mb-10 text-2xl text-white">
         <h1 className='text-4xl text-white' >Men contactman</h1>
-        <p className='text-xl text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
+        <p className='text-2xl text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
 
     </div>
     </div>
