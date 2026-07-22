370ecb8f2fab9383f4321fd2a8d8513791df777a
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:32:58 +0500

    hero star

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ContactPage() {
   return (
     <div >
-      <div className="container w-full bg-amber-800 mb-10 text-2xl text-white">
+      <div className="container h- w-full bg-amber-800 mb-10 text-2xl text-white">
         <h1>Men contactman</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
 
