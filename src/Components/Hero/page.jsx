269b01dc746b5fa6ff1f656d85d5cf4a3bfc6760
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:41:29 +0500

    hero star

--- src/Components/Hero/page.jsx
+++ src/Components/Hero/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero() {
   return (
     <div className='w-full bg-gray-500 h-auto'>
-        <h1>hero start</h1>
+        <h1 className='text-4xl'>hero start</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
         <button>bossish</button>
     </div>
