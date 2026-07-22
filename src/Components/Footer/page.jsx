db8642f81e9a0cb292ba4b25ec15739b152507f1
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:48:19 +0500

    hero star

--- src/Components/Footer/page.jsx
+++ src/Components/Footer/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='w-full'>
+    <div className='w-full h-autog'>
         <h1>Footer qismi</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel sint aspernatur, quaerat rem animi totam omnis ea cum facilis nobis.</p>
     </div>
