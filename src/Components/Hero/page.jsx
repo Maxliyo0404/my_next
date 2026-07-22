85dc912b9125b2ad1e51059323a428a2e039c4da
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:43:12 +0500

    hero star button

--- src/Components/Hero/page.jsx
+++ src/Components/Hero/page.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className='w-full bg-gray-500 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
         <p className='text-2xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
-        <button>bossish</button>
+        <button type='submit' >bossish</button>
     </div>
   )
 }
