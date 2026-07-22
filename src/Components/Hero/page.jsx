0cb41cdc9dc38dbeb42c66a0fbdc662393f42621
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:42:42 +0500

    hero star css style

--- src/Components/Hero/page.jsx
+++ src/Components/Hero/page.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className='w-full bg-gray-500 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
-        <p className='text-xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
+        <p className='text-2xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
         <button>bossish</button>
     </div>
   )
