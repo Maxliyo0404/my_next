06970af975e8cbfb651865f570a75cebc4b90cf8
Mahliyo <gasandigital13@gmail.com>
2026-07-22 23:25:17 +0500

    hero star

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -4,7 +4,7 @@ export default function ServicesPage() {
   return (
     <div>
        <div className="container w-full bg-emerald-800 mb-10 text-2xl text-white">
-        <h1>Men servicesman</h1>
+        <h1 className>Men servicesman</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam labore eaque doloribus.</p>
     </div>
     </div>
