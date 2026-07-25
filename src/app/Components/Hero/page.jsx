48c2d46eb25d9bda2a9844ba1cdeeb1016cd694a
Mahliyo <gasandigital13@gmail.com>
2026-07-25 20:39:11 +0500

    about star

--- src/app/Components/Hero/page.jsx
+++ src/app/Components/Hero/page.jsx
@@ -9,7 +9,7 @@ setName(newNme);
     <div className='w-full bg-gray-900 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
         <p className='text-2xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
-        <button onClick={()=> handChange()} type='submit ' className='bg-blue-600 m-auto px-5 py-6' >bossish</button>
+        <button onClick={()=> handChange("Maxliyo")} type='submit ' className='bg-blue-600 m-auto px-5 py-6' >bossish</button>
     </div>
   )
 }
