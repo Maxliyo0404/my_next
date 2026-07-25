a3b1f79da2e0a73a2cba2afd46811c0768b332f2
Mahliyo <gasandigital13@gmail.com>
2026-07-25 21:42:36 +0500

    sectionOne  star

--- src/app/Components/SectionOne/page.jsx
+++ src/app/Components/SectionOne/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SectionOnepage() {
   return (
     <div className='w-full bg-fuchsia-800 '>
-        <h2 className='text-white'> o'zim haqimda </h2>
+        <h2 className='text-white text-4xl'> o'zim haqimda </h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum perferendis quis dolorum quisquam ipsum omnis error ducimus ut? Earum possimus, iusto, ratione neque labore similique quae velit iure ut error pariatur ad nisi! Tempore, error. Beatae ab magni et nulla architecto debitis veritatis asperiores quia? Quaerat in nobis rem itaque molestiae. Provident earum quas dicta dolore totam voluptates distinctio? Amet, eveniet veniam! Ex praesentium dolorem magnam eos officia dignissimos ut incidunt facere ipsam iure atque cumque repellat, doloribus magni, earum commodi, ad saepe. Eum beatae, culpa, ad enim illo necessitatibus impedit exercitationem totam quod, quos eveniet porro quaerat possimus voluptas.</p>
     </div>
   )
