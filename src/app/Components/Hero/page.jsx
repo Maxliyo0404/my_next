e98ad03bb6b6470eb16862a6d071b84727eb4307
Mahliyo <gasandigital13@gmail.com>
2026-07-25 20:30:07 +0500

    about star

--- src/app/Components/Hero/page.jsx
+++ src/app/Components/Hero/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export default function Hero() {
+  const
   return (
-    <div className='w-full bg-gray-500 h-auto'>
+    <div className='w-full bg-gray-900 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
         <p className='text-2xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
         <button type='submit ' className='bg-blue-600 m-auto px-5 py-6' >bossish</button>
