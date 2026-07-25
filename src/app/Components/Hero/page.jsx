4f265e8e0d6c9e60833fbf0443bc958bc8066d5c
Mahliyo <gasandigital13@gmail.com>
2026-07-25 20:39:39 +0500

    about star

--- src/app/Components/Hero/page.jsx
+++ src/app/Components/Hero/page.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function Hero() {
-  const [name, setName] = useState();
+  const [name, setName] = useState('');
   const handChange = (newNme)=>{
 setName(newNme);
   }
