f41a4c3199665b6382f23de3f70af3e4a8b5e1bd
Mahliyo <gasandigital13@gmail.com>
2026-07-25 20:34:45 +0500

    about star

--- src/app/Components/Hero/page.jsx
+++ src/app/Components/Hero/page.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 export default function Hero() {
   const [name, setName] = useState();
   const handChange = (name)=>{
-
+setName
   }
   return (
     <div className='w-full bg-gray-900 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
         <p className='text-2xl text-white' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus dolores nostrum deleniti laborum, ut suscipit architecto beatae tempore aliquid, quis cupiditate ab non provident modi quo qui optio quam. Soluta!</p>
-        <button type='submit ' className='bg-blue-600 m-auto px-5 py-6' >bossish</button>
+        <button onClick={handChange()=></div>} type='submit ' className='bg-blue-600 m-auto px-5 py-6' >bossish</button>
     </div>
   )
 }
