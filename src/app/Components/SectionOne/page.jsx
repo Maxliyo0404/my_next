7263e08ce253c385de0f5726668525928262a7c8
Mahliyo <gasandigital13@gmail.com>
2026-07-25 21:45:40 +0500

    sectionOne  star

--- src/app/Components/SectionOne/page.jsx
+++ src/app/Components/SectionOne/page.jsx
@@ -4,7 +4,7 @@ function SectionOnepage() {
   return (
     <div className='w-full bg-fuchsia-800 '>
         <h2 className='text-white text-4xl text-center'> o'zim haqimda </h2>
-        <p className='text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum perferendis quis dolorum quisquam ipsum omnis error ducimus ut? Earum possimus, iusto, ratione neque labore similique quae velit iure ut error pariatur ad nisi! Tempore, error. Beatae ab magni et nulla architecto debitis veritatis asperiores quia? Quaerat in nobis rem itaque molestiae. Provident earum quas dicta dolore totam voluptates distinctio? Amet, eveniet veniam! Ex praesentium dolorem magnam eos officia dignissimos ut incidunt facere ipsam iure atque cumque repellat, doloribus magni, earum commodi, ad saepe. Eum beatae, culpa, ad enim illo necessitatibus impedit exercitationem totam quod, quos eveniet porro quaerat possimus voluptas.</p>
+        <p className='text-white text-3xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum perferendis quis dolorum quisquam ipsum omnis error ducimus ut? Earum possimus, iusto, ratione neque labore similique quae velit iure ut error pariatur ad nisi! Tempore, error. Beatae ab magni et nulla architecto debitis veritatis asperiores quia? Quaerat in nobis rem itaque molestiae. Provident earum quas dicta dolore totam voluptates distinctio? Amet, eveniet veniam! Ex praesentium dolorem magnam eos officia dignissimos ut incidunt facere ipsam iure atque cumque repellat, doloribus magni, earum commodi, ad saepe. Eum beatae, culpa, ad enim illo necessitatibus impedit exercitationem totam quod, quos eveniet porro quaerat possimus voluptas.</p>
     </div>
   )
 }
