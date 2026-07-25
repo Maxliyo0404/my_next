408432e6fb40eacf2279c0f1ea8af4a192559aba
Mahliyo <gasandigital13@gmail.com>
2026-07-25 21:40:39 +0500

    sectionOne  star

--- src/app/Components/SectionOne/page.jsx
+++ src/app/Components/SectionOne/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SectionOnepage() {
   return (
-    <div className='w-full bg-fuchsia-800'>
-        .
+    <div className='w-full bg-fuchsia-800 '>
+        <h2></h2>
     </div>
   )
 }
