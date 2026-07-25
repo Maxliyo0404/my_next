083c165e7df1be19a6dcc2ed8971657d98f215fa
Mahliyo <gasandigital13@gmail.com>
2026-07-25 20:31:45 +0500

    about star

--- src/app/Components/Hero/page.jsx
+++ src/app/Components/Hero/page.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 
 export default function Hero() {
-  const [name, setName] = useState()
+  const [name, setName] = useState();
+  const handChange =
   return (
     <div className='w-full bg-gray-900 h-auto'>
         <h1 className='text-4xl text-white'>hero start</h1>
